33bdd0962839eac8db07fc014dc66eb86ade903d
Tigidipoïpoï <a.peyromauredebordbroca@rubika-edu.com>
2016-10-23 12:08:38 +0200

    1st commit for Tigidi.

---
+++ Assets/Scripts/Interacts/BumpRespawnSet.cs
@@ -0,0 +1,24 @@
+﻿//Original Scripts by IIColour (IIColour_Spectrum)
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class BumpRespawnSet : MonoBehaviour
+{
+
+    public Vector3 respawnPositionOffset;
+    public int respawnDirection;
+    public string respawnText;
+
+    private IEnumerator bump()
+    {
+        SaveData.currentSave.respawnScenePosition = new SeriV3(transform.position + respawnPositionOffset);
+        SaveData.currentSave.respawnSceneDirection = respawnDirection;
+        SaveData.currentSave.respawnText = respawnText;
+        //Old:  Application.loadedLevelName
+        SaveData.currentSave.respawnSceneName = SceneManager.GetActiveScene().name;
+        yield return null;
+    }
+
+}
